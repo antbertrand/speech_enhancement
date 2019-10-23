ad3acfbcb7f288f11cff3abf896abc8d5065b5ce
Paul-Aime <paul.aime@grenoble-inp.org>
2019-10-23 09:34:54 +0200

    add nist2wav.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import os
 import torch
+import torchaudio
+from scipy.io import wavfile
 
 def main():
-	pass
+    filepath = 'data/timit_test/DR1/FAKS0/SA1.WAV'
+
+    assert os.path.isfile(filepath)
+
+    data, fs = torchaudio.load_wav(filepath)
+
+    print(data)
+    print(type(data))
+    print(data.shape)
+    print(fs)
+
+
+    # fs, data = wavfile.read(filepath)
+
+    # print(fs)
+    # print(data)
+
 
 
 if __name__ == '__main__':
-	main()
+    main()  
---
+++ nist2wav.py
@@ -0,0 +1,32 @@
+import os
+from sphfile import SPHFile
+
+
+def main():
+    
+    root_dir = "./data"
+
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".WAV"):
+                wav_file = os.path.join(root, file)
+                nist2wav(wav_file)
+
+
+def nist2wav(wav_file):
+
+    sph = SPHFile(wav_file)
+    txt_file = ""
+    txt_file = wav_file[:-3] + "TXT"
+
+    f = open(txt_file,'r')
+    for line in f:
+        words = line.split(" ")
+        start_time = (int(words[0])/16000)
+        end_time = (int(words[1])/16000)
+    print("writing file ", wav_file)
+    sph.write_wav(wav_file.replace(".WAV",".wav"),start_time,end_time)
+
+
+if __name__ == '__main__':
+    main()
