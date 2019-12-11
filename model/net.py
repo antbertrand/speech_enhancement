8e593e67704f3eec177d3a9caabbdd06b5b8ec5c
abert1 <abertrand@numericube.com>
2019-12-11 17:29:43 +0100

    Not much

--- model/net.py
+++ model/net.py
@@ -1,30 +1,37 @@
 import torch
 import torch.nn.functional as F
 
+def conv2d(x, W):
+    '''1 dimentional convolution defined in the paper
+    the function's name is not appropriate and
+    we didn't change that'''
+    return torch.nn.Conv2d(x, W, strides=[1, 100, 1, 1], padding='SAME')
+
+
 class CNN(torch.nn.Module):
-    
+
     #Our batch shape for input x is (3, 32, 32)
-    
-    def __init__(self, batch_size, NEFF, N_IN, N_OUT, DECAY=0.999)::
+
+    def __init__(self, batch_size, NEFF, N_IN, N_OUT, DECAY=0.999):
         '''NEFF: number of effective FFT points
         N_IN: number of input frames into the nets
         N_OUT: only tested for 1, errors may occur for other number
         DECAY: decay for global mean and var estimation using batch norm
         '''
-        
+
         super(CNN, self).__init__()
-        
+
         self.batch_size = batch_size
         self.NEFF = NEFF
         self.N_IN = N_IN
         self.N_OUT = N_OUT
         self.DECAY = DECAY
-        
+
         self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
         self.fc1 = torch.nn.Linear(18 * 16 * 16, 64)
-        
+
     def forward(self, x):
-        
+
         x = self.conv1(x)
         x = self.fc1(x)
         return x
